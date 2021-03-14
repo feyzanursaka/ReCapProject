ef331d0ee4cfb000cb732a362b10a5680abb9995
feyzanursaka1996@gmail.com <feyzanursaka1996@gmail.com>
2021-03-15 00:50:12 +0200

    Ödev 1 - Autofac

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -18,16 +20,11 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
-
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
             //bussiness codes
 
-            if (car.CarName.Length < 2)
-            {
-                //magic string
-                return new ErrorResult(Messages.CarNameInValid);
-            }
             _carDal.Add(car);
             return new SuccessResult(Messages.CarAdded);
         }
